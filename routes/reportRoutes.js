3a890d6051005ac8fb788a8984e0c0f87321f89d
Shreyam <shreyam@example.com>
2026-06-17 23:18:07 +0530

    feat: implement reports GET and POST APIs using Supabase

--- routes/reportRoutes.js
+++ routes/reportRoutes.js
@@ -1,19 +1,25 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+const middleware = require('../middleware');
+const { reportController } = require('../controllers');
 
 // GET all reports
-router.get("/");
+router.get('/', middleware.authMiddleware, reportController.getReports);
 
 // GET report by ID
-router.get("/:id");
+router.get('/:id');
 
 // CREATE report
-router.post("/");
+router.post(
+  '/',
+  middleware.authMiddleware,
+  reportController.createReport
+);
 
 // UPDATE report
-router.put("/:id");
+router.put('/:id');
 
 // DELETE report
-router.delete("/:id");
+router.delete('/:id');
 
 module.exports = router;
